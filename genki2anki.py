764e5339d86303995f20dd18eb3f5dfc863502f7
Josh Matthews <jmatthews492@gmail.com>
2017-03-08 11:58:14 -0500

    Split anki utils into a separate file

--- genki2anki.py
+++ genki2anki.py
@@ -3,8 +3,6 @@
 
 import argparse
 import sqlite3
-import random
-import string
 import os
 import sys
 import re
@@ -12,8 +10,8 @@ import time
 import shutil
 import json
 import zipfile
+import ankiutils
 from PIL import Image
-from hashlib import sha1
 
 
 # TODO: don't hardcode this
@@ -45,12 +43,12 @@ def create_base_apkg(cards, genki_dir, output_path):
 
     apkg_path = 'genki_apkg'
     if os.path.isfile(apkg_path):
-        print('%s exists and is not a directory')
+        print('%s exists and is not a directory' % apkg_path)
         sys.exit(1)
     if not os.path.exists(apkg_path):
         os.mkdir(apkg_path)
     if len(os.listdir(apkg_path)) > 0:
-        print('%s exists but is not empty')
+        print('%s exists but is not empty' % apkg_path)
         sys.exit(1)
 
     connection = sqlite3.connect(
@@ -77,8 +75,8 @@ def create_base_apkg(cards, genki_dir, output_path):
 
 
 def create_card(card, anki_conn, genki_dir, apkg_path, media):
-    ts_id = timestampID(anki_conn, 'notes')
-    guid = guid64()
+    ts_id = ankiutils.timestampID(anki_conn, 'notes')
+    guid = ankiutils.guid64()
     english = card['ENG_WORD']
     japanese = card['JPN_WORD']
     kanji_image_name = card['JPN_WORD_IMAGE']
@@ -87,7 +85,7 @@ def create_card(card, anki_conn, genki_dir, apkg_path, media):
     illustration_tag = ""
     if illustration_name is not None:
         illustration_tag = '<img src="%s" />' % illustration_name
-    tags = ' genki-%s ' %  card['LESSON']
+    tags = ' genki-%s ' % card['LESSON']
     if card['PARTS']:
         tags += '%s ' % card['PARTS'].replace(' ', '-')
     flds = (
@@ -114,11 +112,11 @@ def create_card(card, anki_conn, genki_dir, apkg_path, media):
             'tags': tags,
             'flds': flds,
             'front': english,
-            'csum': fieldChecksum(english)
+            'csum': ankiutils.fieldChecksum(english)
         }
     )
 
-    cards_tsid = timestampID(anki_conn, 'cards')
+    cards_tsid = ankiutils.timestampID(anki_conn, 'cards')
     anki_conn.execute(
         """INSERT INTO "cards" VALUES(:tsid,:nid,:did,0,:mod,0,0,0,76,0,0,0,0,0,0,0,0,'');""",
         {
@@ -171,102 +169,6 @@ def create_zip(indir, outfile):
     for f in os.listdir(indir):
         zf.write(os.path.join(indir, f), arcname=f)
     zf.close()
-
-
-#######################################################
-#  Helper functions copied from the Anki source code  #
-#######################################################
-
-def intTime(scale=1):
-    "The time in integer seconds. Pass scale=1000 to get milliseconds."
-    return int(time.time()*scale)
-
-
-def timestampID(db, table):
-    "Return a non-conflicting timestamp for table."
-    # be careful not to create multiple objects without flushing them, or they
-    # may share an ID.
-    t = intTime(1000)
-    while db.execute("select id from %s where id = ?" % table, (t,)).fetchone() is not None:
-        t += 1
-    return t
-
-
-# used in ankiweb
-def base62(num, extra=""):
-    s = string; table = s.ascii_letters + s.digits + extra
-    buf = ""
-    while num:
-        num, i = divmod(num, len(table))
-        buf = table[i] + buf
-    return buf
-
-
-def base91(num):
-    _base91_extra_chars = "!#$%&()*+,-./:;<=>?@[]^_`{|}~"
-    # all printable characters minus quotes, backslash and separators
-    return base62(num, _base91_extra_chars)
-
-
-def guid64():
-    "Return a base91-encoded 64bit random number."
-    return base91(random.randint(0, 2**64-1))
-
-
-reStyle = re.compile("(?si)<style.*?>.*?</style>")
-reScript = re.compile("(?si)<script.*?>.*?</script>")
-reTag = re.compile("<.*?>")
-reEnts = re.compile("&#?\w+;")
-reMedia = re.compile("(?i)<img[^>]+src=[\"']?([^\"'>]+)[\"']?[^>]*>")
-
-def entsToTxt(html):
-    # entitydefs defines nbsp as \xa0 instead of a standard space, so we
-    # replace it first
-    html = html.replace("&nbsp;", " ")
-    def fixup(m):
-        text = m.group(0)
-        if text[:2] == "&#":
-            # character reference
-            try:
-                if text[:3] == "&#x":
-                    return chr(int(text[3:-1], 16))
-                else:
-                    return chr(int(text[2:-1]))
-            except ValueError:
-                pass
-        else:
-            # named entity
-            try:
-                text = chr(name2codepoint[text[1:-1]])
-            except KeyError:
-                pass
-        return text # leave as is
-    return reEnts.sub(fixup, html)
-
-
-def stripHTML(s):
-    s = reStyle.sub("", s)
-    s = reScript.sub("", s)
-    s = reTag.sub("", s)
-    s = entsToTxt(s)
-    return s
-
-
-def stripHTMLMedia(s):
-    "Strip HTML but keep media filenames"
-    s = reMedia.sub(" \\1 ", s)
-    return stripHTML(s)
-
-
-def checksum(data):
-    if isinstance(data, str):
-        data = data.encode("utf-8")
-    return sha1(data).hexdigest()
-
-
-def fieldChecksum(data):
-    # 32 bit unsigned number from first 8 digits of sha1 hash
-    return int(checksum(stripHTMLMedia(data).encode("utf-8"))[:8], 16)
 
 
 if __name__ == '__main__':
